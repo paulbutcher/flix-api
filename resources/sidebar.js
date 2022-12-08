645b28bf3e51102b4641664dabb0be7939cd4d62
Paul Butcher <paul@paulbutcher.com>
2022-12-08 22:03:38 +0000

    Towards single file

--- resources/sidebar.js
+++ resources/sidebar.js
@@ -1,6 +1,6 @@
-const buttons = document.querySelectorAll('.accordion-collapse')
-buttons.forEach(el => {
-  el.addEventListener("show.bs.collapse", _ => {
-    window.open(el.getAttribute("data-href"), "main")
-  })
-})
+// const buttons = document.querySelectorAll('.accordion-collapse')
+// buttons.forEach(el => {
+//   el.addEventListener("show.bs.collapse", _ => {
+//     window.open(el.getAttribute("data-href"), "main")
+//   })
+// })
