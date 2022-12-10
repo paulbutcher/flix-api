c847dfac22c9cb578a75c670b31344c53602317a
Paul Butcher <paul@paulbutcher.com>
2022-12-10 15:43:48 +0000

    Show and hide namespaces

--- resources/sidebar.js
+++ resources/sidebar.js
@@ -1,9 +1,27 @@
-const buttons = document.querySelectorAll('.accordion-collapse')
-buttons.forEach(el => {
-  el.addEventListener("show.bs.collapse", _ => {
-    document.querySelector("#" + CSS.escape(el.getAttribute("data-ns"))).scrollIntoView()
-  })
+var defaultNamespace = document.querySelector("#Prelude")
+defaultNamespace.classList.add("active")
+var activeNamespaces = new Set()
+
+document.querySelectorAll('.accordion-collapse').forEach(el => {
+  let ns = document.querySelector("#" + CSS.escape(el.getAttribute("data-ns")))
+  el.addEventListener("show.bs.collapse", _ => makeNsActive(ns))
+  el.addEventListener("hide.bs.collapse", _ => makeNsInactive(ns))
 })
+
+function makeNsActive(ns) {
+  activeNamespaces.add(ns)
+  if(!activeNamespaces.has(defaultNamespace))
+    defaultNamespace.classList.remove("active")
+  ns.classList.add("active")
+  ns.scrollIntoView()
+}
+
+function makeNsInactive(ns) {
+  activeNamespaces.delete(ns)
+  ns.classList.remove("active")
+  if(activeNamespaces.size == 0)
+    defaultNamespace.classList.add("active")
+}
 
 function showDoc(id) {
   document.querySelector("#" + CSS.escape(id)).scrollIntoView({behavior: "smooth", block: "center"})
