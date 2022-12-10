5d1159e1fbd72138359f2845745e4234b19de9f3
Paul Butcher <paul@paulbutcher.com>
2022-12-10 14:44:36 +0000

    Scrolling now working

--- resources/sidebar.js
+++ resources/sidebar.js
@@ -1,6 +1,10 @@
-// const buttons = document.querySelectorAll('.accordion-collapse')
-// buttons.forEach(el => {
-//   el.addEventListener("show.bs.collapse", _ => {
-//     window.open(el.getAttribute("data-href"), "main")
-//   })
-// })
+const buttons = document.querySelectorAll('.accordion-collapse')
+buttons.forEach(el => {
+  el.addEventListener("show.bs.collapse", _ => {
+    document.querySelector("#" + CSS.escape(el.getAttribute("data-ns"))).scrollIntoView()
+  })
+})
+
+function showDoc(id) {
+  document.querySelector("#" + CSS.escape(id)).scrollIntoView({behavior: "smooth", block: "center"})
+}
