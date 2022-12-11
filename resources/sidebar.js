539fc850721820757098640c1f87b386b5b8b051
Paul Butcher <paul@paulbutcher.com>
2022-12-11 11:26:16 +0000

    Search

--- resources/sidebar.js
+++ resources/sidebar.js
@@ -2,15 +2,24 @@ const defaultNamespace = document.querySelector("#Prelude")
 defaultNamespace.classList.add("active")
 const activeNamespaces = new Set()
 
-document.querySelectorAll('.accordion-collapse').forEach(el => {
-  let ns = document.querySelector("#" + CSS.escape(el.getAttribute("data-ns")))
-  el.addEventListener("show.bs.collapse", _ => makeNsActive(ns))
-  el.addEventListener("hide.bs.collapse", _ => makeNsInactive(ns))
+const namespaces = new Map()
+
+document.querySelectorAll('.accordion-item').forEach(item => {
+  let ns = document.querySelector("#" + CSS.escape(item.getAttribute("data-ns")))
+  let collapse = item.querySelector(".accordion-collapse")
+  collapse.addEventListener("show.bs.collapse", _ => makeNsActive(ns))
+  collapse.addEventListener("hide.bs.collapse", _ => makeNsInactive(ns))
+  let links = []
+  item.querySelectorAll(".sidebar-link").forEach(link => {
+    link.onclick = _ => showDoc(link.getAttribute("data-link"))
+    links.push([link, link.getAttribute("data-name")])
+  })
+  namespaces.set(item, [ns, links])
 })
 
 function makeNsActive(ns) {
   activeNamespaces.add(ns)
-  if(!activeNamespaces.has(defaultNamespace))
+  if (!activeNamespaces.has(defaultNamespace))
     defaultNamespace.classList.remove("active")
   ns.classList.add("active")
   ns.scrollIntoView()
@@ -19,7 +28,7 @@ function makeNsActive(ns) {
 function makeNsInactive(ns) {
   activeNamespaces.delete(ns)
   ns.classList.remove("active")
-  if(activeNamespaces.size == 0) {
+  if (activeNamespaces.size == 0) {
     defaultNamespace.classList.add("active")
     defaultNamespace.scrollIntoView()
   }
@@ -30,8 +39,35 @@ var activeDoc = null
 function showDoc(id) {
   let el = document.querySelector("#" + CSS.escape(id))
   el.scrollIntoView({behavior: "smooth", block: "center"})
-  if(activeDoc)
+  if (activeDoc)
     activeDoc.classList.remove("active")
   activeDoc = el
   el.classList.add("active")
+}
+
+function onSearch(el) {
+  let searchTerm = el.value
+  if (searchTerm.length > 0) {
+    namespaces.forEach(([ns, links], item) => {
+      item.querySelector(".accordion-collapse").style.display = "block"
+      item.querySelector(".accordion-button").classList.add("disabled")
+      var found = false
+      links.forEach(([l, n]) => {
+        if (n.includes(searchTerm)) {
+          found = true
+          l.style.display = "block"
+        } else {
+          l.style.display = "none"
+        }
+      })
+      item.style.display = found ? "block" : "none"
+      ns.style.display = found ? "block" : "none"
+    })
+  } else {
+    document.querySelectorAll(".accordion-collapse").forEach(el => el.removeAttribute("style"))
+    document.querySelectorAll(".accordion-button").forEach(el => el.classList.remove("disabled"))
+    document.querySelectorAll(".accordion-item").forEach(el => el.removeAttribute("style"))
+    document.querySelectorAll(".sidebar-link").forEach(el => el.removeAttribute("style"))
+    document.querySelectorAll(".nspace").forEach(el => el.removeAttribute("style"))
+  }
 }
